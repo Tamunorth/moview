f623eea9a78733bdff1dc4e582235d93f21346b9
Tamunorth <daviesmanuelfavour@gmail.com>
2024-04-14 19:25:25 +0100

    skeleton

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  const name = "1";
+
+  return (
+    <div className="App">
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+export default App;
